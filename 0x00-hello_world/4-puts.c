c98e734c2b943428e71f434cc1258dfa1cf06a65
AyeboganIdowu <ayeboganidowu@gmail.com>
2022-12-11 05:18:44 -0800

    Task 4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+  puts("\"Programming is like buidling a multile langauage puzzle\n");
+  return (0);
+}
