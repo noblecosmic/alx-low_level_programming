25488512364bc934eb9f2abde8355f52cd310c2e
AyeboganIdowu <ayeboganidowu@gmail.com>
2022-12-13 13:24:04 -0800

    Task 3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - a function that checks for lowercase character
- * 
+ * c is single letter
  * Return: return 1 if int c is lowercase, return 0 if otherwise
  */
 int _islower(int c)
