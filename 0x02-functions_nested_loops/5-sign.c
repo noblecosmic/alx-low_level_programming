6085baf2b05dd2dbc9fc18f5da16d99c82de7794
AyeboganIdowu <ayeboganidowu@gmail.com>
2022-12-14 03:17:39 -0800

    Task 5. sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,25 +2,24 @@
 
 /**
  * print_sign - a function that prints the sign of a number
- * @n: n is a number
- * Return: return 1 prints '+' if n > 0, if 0 prints '0' if n = 0, else -1 prints '-' if n < 0
+ * @n: n is a number that will be printed
+ * Return: return 1 print '+' if n > 0, if 0 print '0' if n = 0, else -1 print'_' if n < 0
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar ('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar ('0');
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		_putchar ('-');
+		_putchar('-');
 		return (-1);
 	}
-	_putchar('\n');
 }
