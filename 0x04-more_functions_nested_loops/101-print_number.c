af8231b2ca7ad519e1541bb84ab82571a8170fe9
AyeboganIdowu <ayeboganidowu@gmail.com>
2022-12-16 17:54:35 -0800

    Task 12.Numbers have life; they're not just symbols on paper

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,21 +1,26 @@
-#include "main.h"
+#include"main.h"
 
 /**
  * print_number - Prints an integer.
- * @n: The integer to be printed.
- */
+ *
+ * @n: The integer to be printed (output).
+*/
+
 void print_number(int n)
 {
 	unsigned int num = n;
 
+	/*check if it's negative*/
 	if (n < 0)
 	{
-		_putchar('-');
+		putchar('-');
 		num = -num;
 	}
 
+	/*then print the first few digits*/
 	if ((num / 10) > 0)
 		print_number(num / 10);
 
-	_putchar((num % 10) + '0');
+	/*Then print the last digit*/
+	putchar((num % 10) + 48);
 }
