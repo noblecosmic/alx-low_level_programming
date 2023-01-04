165466e570e30b608620325920f2b9b76ff6190f
AyeboganIdowu <ayeboganidowu@gmail.com>
2023-01-04 05:25:41 -0800

    Task 0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,12 +8,10 @@ void _puts_recursion(char *s)
 {
 	if (*s == 0)
 	{
-		_putchar(*s);
-		s++;
-		_puts_recursion(s);
-	}
-	{
 		_putchar('\n');
 		return;
 	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
