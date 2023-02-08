6e7500ec0dc2c692fb5bca492b6f18da0e181662
AyeboganIdowu <ayeboganidowu@gmail.com>
2023-02-08 02:07:58 -0800

    Task 0. Print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,15 @@
-#ifndef MAIN
-#define MAIN
+#ifndef LISTS_H
+#define LISTS_H
+
+/*
+ * File: lists.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes and definitions for all functions
+ *       and types written in the 0x11-singly_linked_lists directory.
+ */
+
+#include <stdlib.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -7,7 +17,6 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct list_s
 {
@@ -15,13 +24,11 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
-int _putchar(char c);
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-int _strlen(char *s);
-list_t *get_last(list_t **head);
-list_t *get_last_node(list_t *head);
-#endif
+
+#endif /* LISTS_H */
